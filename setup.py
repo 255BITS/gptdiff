44e404df1e28567fa68d7482e1d13c1b0e7b14b7
Mikkel Garcia <mikkel@255bits.com>
2025-01-28 09:23:06 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gptdiff',
-    version='0.2.1',
+    version='0.1.0',
     description='A tool to generate and apply git diffs using LLMs',
     author='255labs',
     packages=find_packages(),  # Use find_packages() to automatically discover packages
@@ -19,6 +19,11 @@ setup(
     entry_points={
         'console_scripts': ['gptdiff=gptdiff.gptdiff:main'],
     },
-    license='MIT',  # Specify the license type (e.g., MIT, Apache 2.0, etc.)
-    license_files=('LICENSE.txt',),  # Include the license file(s)
+    license=None, # Remove license argument
+    # license_file='LICENSE.txt', # Remove license_file argument
+    classifiers=[  # Add license classifiers
+        'License :: OSI Approved :: MIT License', # Standard MIT license classifier
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
 )
