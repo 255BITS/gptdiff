cc6cabada7e243365b868f9614645e38353e1b03
Martyn Garcia <martyn.garcia@gmail.com>
2025-02-02 13:16:49 -0700

    update exports

--- gptdiff/__init__.py
+++ gptdiff/__init__.py
@@ -1,3 +1,3 @@
-from .gptdiff import generate_diff, smartapply
+from .gptdiff import generate_diff, smartapply, load_project_files, build_environment
 
-__all__ = ['generate_diff', 'smartapply']
+__all__ = ['generate_diff', 'smartapply', 'load_project_files', 'build_environment']
